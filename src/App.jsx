7d7068cb637e7be53ebc77391b2c4b72d8655e0c
Miles Ryker Odazie <rykerodazie@gmail.com>
2024-04-04 15:33:01 +0100

    login added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
+import { useState } from "react";
 import "./App.css";
+import React, { useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const base_url = import.meta.env.VITE_API_BASE_URL;
+  const [message, setMessage] = useState("");
+  console.log("the refresh message => ", message);
+  // axios.get(API_SERVER + '/todos', { withCredentials: true })
+  // axios.defaults.withCredentials = true;
+  const validResponse = async () => {
+    const response = await axios.get(`${base_url}/auth/validate-token`, {
+      withCredentials: true,
+    });
+    console.log("the response from valid => ", response);
+    if (response?.data?.valid) {
+      setMessage(response.data.message);
+    } else {
+      setMessage("validation of token did not work");
+    }
+  };
+
+  useEffect(() => {
+    validResponse();
+  }, []);
+
   return (
     <div className="h-screen">
       <p>This is he home page</p>
+      <p>{message && message}</p>
     </div>
   );
 }
--- src/components/login-form.jsx
+++ src/components/login-form.jsx
@@ -1,17 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const LoginForm = () => {
+  const navigate = useNavigate();
+  const base_url = import.meta.env.VITE_API_BASE_URL;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  //axios with credentials
+  axios.defaults.withCredentials = true;
+
+  //handle submit
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    axios
+      .post(`${base_url}/auth/login`, {
+        email: email,
+        password: password,
+      })
+      .then((res) => {
+        console.log("the login sussess response => ", res);
+        if (res.data.success) {
+          navigate("/");
+        }
+      })
+      .catch((error) => {
+        if (error instanceof axios.AxiosError) {
+          console.log("the error => ", error?.response?.data);
+        }
+      })
+      .finally(() => setIsLoading(false));
+  };
   return (
     <div className="max-w-2xl mx-auto py-20">
       <div className=" bg-white drop-shadow-md p-5 w-full">
         <h1 className="text-center font-semibold">Login Form</h1>
-        <form className="space-y-3">
+        <form onSubmit={handleSubmit} className="space-y-3">
           <div className="flex flex-col space-y-1">
             <label htmlFor="">Email</label>
             <input
               type="email"
               placeholder="Email"
               required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               className="w-full px-3 py-2 outline-none border border-gray-400 rounded-md"
             />
           </div>
@@ -21,10 +56,16 @@ const LoginForm = () => {
               type="password"
               placeholder="Password"
               required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
               className="w-full px-3 py-2 outline-none border border-gray-400 rounded-md"
             />
           </div>
-          <button className="bg-gray-300 px-5 py-2 rounded-md font-medium">
+          <button
+            disabled={isLoading}
+            type="submit"
+            className="bg-gray-300 px-5 py-2 rounded-md font-medium disabled:opacity-40 disabled:cursor-not-allowed"
+          >
             Login
           </button>
         </form>
